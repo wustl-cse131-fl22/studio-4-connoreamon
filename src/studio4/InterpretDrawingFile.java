365cfbf486e45d432f98b34350b8e5b8d3ff3ac7
16189 <16189@DESKTOP-2Q007KP.dhcp.wustl.edu>
2022-10-12 15:21:15 -0500

    Studio 4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		if (shape.equals("rectangle")){
+			double width = in.nextDouble();
+			double height = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(red, green, blue);
+			if( isFilled) {
+				StdDraw.filledRectangle(width, height, halfWidth, halfHeight);
+			} else {
+				StdDraw.rectangle(width, height, halfWidth, halfHeight);
+			}	
+		}
+		if (shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+			StdDraw.setPenColor(red, green, blue);
+			if( isFilled) {
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			} else {
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+		}
+		if (shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double [] x = {x1, x2, x3};
+			double [] y = {y1, y2, y3};
+			StdDraw.setPenColor(red, green, blue);
+			if( isFilled) {
+				StdDraw.filledPolygon(x, y);
+			} else {
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
